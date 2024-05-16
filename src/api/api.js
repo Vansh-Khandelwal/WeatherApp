e8511673b06138ba85b83bd12fa508d2e357d8b9
Vansh <vansh.khandelwal06@gmail.com>
2024-05-16 07:54:20 +0530

    Read Me

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,3 @@
 import axios from 'axios'
 
-// export const API = (city, country) => axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city + ',' + country}&appid=31aaf3aec453ee4eaa03b842b89f639f&units=metric`);
-
 export const API = (city, country) => axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city + ',' + country}&appid=${process.env.REACT_APP_API_TOKEN}&units=metric`);
